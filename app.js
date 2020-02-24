86684de911c1f7c57a5e34530e545862e00bfef6
Samuel H <flashfireex@gmail.com>
2020-02-24 18:37:26 -0500

    Update port to work on server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5000;
 const superagent = require('superagent');
 const fs = require('fs');
 const download = require('image-downloader');
