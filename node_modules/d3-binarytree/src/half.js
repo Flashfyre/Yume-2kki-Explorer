b0a0af239f102a7456a718bc0b617b29f5369ecf
Samuel H <flashfireex@gmail.com>
2020-02-24 20:04:42 -0500

    Remove node modules

--- node_modules/d3-binarytree/src/half.js
+++
@@ -1,5 +0,0 @@
-export default function(node, x0, x1) {
-  this.node = node;
-  this.x0 = x0;
-  this.x1 = x1;
-}
--- node_modules/d3-octree/src/z.js
+++
@@ -1,7 +0,0 @@
-export function defaultZ(d) {
-  return d[2];
-}
-
-export default function(_) {
-  return arguments.length ? (this._z = _, this) : this._z;
-}
