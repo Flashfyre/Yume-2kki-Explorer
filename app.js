3520a20eb66e4cf0530e60ade53f8a5ea522144c
Samuel H <flashfireex@gmail.com>
2020-02-29 17:22:04 -0500

    Add maps and new conn types
    Add map processing to calculate map size with configuration for size difference; add support for chance and locked condition connections; add modded force graph library for specific changes not supported by the current build

--- app.js
+++ app.js
@@ -1,62 +1,89 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { exec } = require("child_process");
 const _ = require('lodash');
 const superagent = require('superagent');
 const fs = require('fs');
+const xmlJs = require('xml-js');
 const download = require('image-downloader');
 const mysql = require("mysql");
-const ConnType = require("./public/js/connType.js").ConnType;
-
-var pool;
-if (process.env.DATABASE_URL) {
-    let dbUrl = process.env.DATABASE_URL.slice(process.env.DATABASE_URL.indexOf("mysql://") + 8);
-    let user = dbUrl.slice(0, dbUrl.indexOf(":"));
-    let password = dbUrl.slice(dbUrl.indexOf(":") + 1, dbUrl.indexOf("@"));
-    let host = dbUrl.slice(dbUrl.indexOf("@") + 1, dbUrl.indexOf("/"));
-    let database = dbUrl.slice(dbUrl.indexOf("/") + 1, dbUrl.indexOf("?") > -1 ? dbUrl.indexOf("?") : dbUrl.length);
-    pool = mysql.createPool({
-        host: host,
-        user: user,
-        password: password,
-        database: database
-    });
-} else {
-    const dbConfig = require("./config/db.config.js");
-
-    pool = mysql.createPool({
-        host: dbConfig.HOST,
-        user: dbConfig.USER,
-        password: dbConfig.PASSWORD,
-        database: dbConfig.DB
-    });
-}
+const ConnType = require("./public/js/conn-type.js").ConnType;
 
 module.exports = pool;
 
-pool.query(`CREATE TABLE IF NOT EXISTS worlds (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    depth INT NOT NULL,
-    filename VARCHAR(255) NOT NULL
-)`);
+var pool;
 
-pool.query(`CREATE TABLE IF NOT EXISTS conns (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sourceId INT NOT NULL,
-    targetId INT NOT NULL,
-    type TINYINT NOT NULL,
-    CONSTRAINT fk_sourceId
-        FOREIGN KEY (sourceId) 
-        REFERENCES world(id),
-    CONSTRAINT fk_targetId
-        FOREIGN KEY (targetId) 
-        REFERENCES world(id)
-)`);
+initDb();
+
+function initDb() {
+    pool = getConnPool();
+
+    pool.query(`CREATE TABLE IF NOT EXISTS worlds (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        depth INT NOT NULL,
+        filename VARCHAR(255) NOT NULL
+    )`);
+    
+    pool.query(`CREATE TABLE IF NOT EXISTS conns (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        sourceId INT NOT NULL,
+        targetId INT NOT NULL,
+        type SMALLINT NOT NULL,
+        CONSTRAINT fk_sourceId
+            FOREIGN KEY (sourceId) 
+            REFERENCES world(id),
+        CONSTRAINT fk_targetId
+            FOREIGN KEY (targetId) 
+            REFERENCES world(id)
+    )`);
+
+    pool.query(`CREATE TABLE IF NOT EXISTS maps (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        worldId INT NOT NULL,
+        mapId CHAR(4) NOT NULL,
+        width INT NOT NULL,
+        height INT NOT NULL,
+        special BIT NOT NULL,
+        FOREIGN KEY (worldId)
+            REFERENCES worlds (id)
+            ON DELETE CASCADE
+    )`);
+
+    pool.end();
+}
+
+function getConnPool() {
+    var ret;
+    if (process.env.DATABASE_URL) {
+        let dbUrl = process.env.DATABASE_URL.slice(process.env.DATABASE_URL.indexOf("mysql://") + 8);
+        let user = dbUrl.slice(0, dbUrl.indexOf(":"));
+        let password = dbUrl.slice(dbUrl.indexOf(":") + 1, dbUrl.indexOf("@"));
+        let host = dbUrl.slice(dbUrl.indexOf("@") + 1, dbUrl.indexOf("/"));
+        let database = dbUrl.slice(dbUrl.indexOf("/") + 1, dbUrl.indexOf("?") > -1 ? dbUrl.indexOf("?") : dbUrl.length);
+        ret = mysql.createPool({
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        });
+    } else {
+        const dbConfig = require("./config/db.config.js");
+    
+        ret = mysql.createPool({
+            host: dbConfig.HOST,
+            user: dbConfig.USER,
+            password: dbConfig.PASSWORD,
+            database: dbConfig.DB
+        });
+    }
+    return ret;
+}
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile('index.html', { root: '.' }))
+app.get('/', (_, res) => res.sendFile('index.html', { root: '.' }))
 
 const startLocation = "Urotsuki's Room";
 
@@ -65,11 +92,18 @@ const batchSize = 20;
 app.get('/worlds', function(req, res) {
     let callback = function (worldData) {
         res.json(worldData);
+        pool.end();
     };
+    pool = getConnPool();
     if (req.query.hasOwnProperty("update") && req.query.update) {
         populateWorldData(function (worldData) {
-            console.log(worldData);
-            getWorldData(callback);
+            if (worldData.length)
+                updateMaps(worldData, 0, function() {
+                    console.log(worldData);
+                    getWorldData(callback);
+                });
+            else
+                getWorldData(callback);
         });
     } else {
         getWorldData(callback);
@@ -86,7 +120,8 @@ function populateWorldData(callback) {
         let worlds = data.query.categorymembers;
         populateWorldDataSub(worldData, worlds, 0, worlds.length <= batchSize, function () {
             updateWorldDepths(worldData, function() {
-                deleteRemovedWorlds(callback);
+                deleteRemovedWorlds();
+                callback(worldData);
             });
         });
     });
@@ -117,7 +152,14 @@ function getWorldData(callback) {
                     type: row.type
                 });
             }
-            callback(worldData);
+            pool.query('SELECT w.id, ROUND(SUM((m.width * m.height) / mwm.worldCount)) AS size FROM maps m JOIN worlds w ON w.id = m.worldId JOIN (SELECT mw.mapId, COUNT(DISTINCT mw.worldId) worldCount FROM maps mw GROUP BY mw.mapId) mwm ON mwm.mapId = m.mapId WHERE m.special = 0 IS NOT NULL GROUP BY w.id', (err, rows) => {
+                if (err) throw err;
+                for (var r in rows) {
+                    let row = rows[r];
+                    worldDataById[row.id].size = row.size;
+                }
+                callback(worldData);
+            });
         });
     });
 }
@@ -377,7 +419,7 @@ function calcDepth(worldData, depthMap, world, depth) {
         let conn = currentWorld.connections[c];
         let w = conn.location;
         if (worldNames.indexOf(w) > -1) {
-            if (conn.type & ConnType.NO_ENTRY)
+            if (conn.type & ConnType.NO_ENTRY || conn.type & ConnType.LOCKED || conn.type & ConnType.DEAD_END || conn.type & ConnType.ISOLATED)
                 continue;
             let d = depthMap[w];
             if (d === -1 || d > depth + 1) {
@@ -413,10 +455,88 @@ function updateWorldsOfDepth(worldsByDepth, d, callback) {
     });
 }
 
-function deleteRemovedWorlds(callback) {
+function deleteRemovedWorlds() {
     pool.query('DELETE w FROM worlds w WHERE NOT EXISTS(SELECT c.id FROM conns c WHERE w.id IN (c.sourceId, c.targetId))', (err, _) => {
-        if (err) console.log(err);
-        callback();
+        if (err) return console.log(err);
+    });
+}
+
+function updateMaps(worldData, w, callback) {
+    let newMapIds = [];
+    let existingMapIds = [];
+    let removedIds = [];
+    let mapIds = worldData[w].mapIds;
+    pool.query(`SELECT id, mapId FROM maps WHERE worldId = ${worldData[w].id}`, (err, rows) => {
+        if (err) return console.log(err);
+        for (var r in rows) {
+            if (mapIds.indexOf(rows[r].mapId) > -1) {
+                existingMapIds.push(rows[r].mapId);
+            } else {
+                removedIds.push(rows[r].id);
+            }
+        }
+        _.map(mapIds, (id) => {
+            if (existingMapIds.indexOf(id) === -1) {
+                newMapIds.push(id);
+            }
+        });
+        var processCallback = function () {
+            if (++w < worldData.length)
+                updateMaps(worldData, w, callback);
+            else
+                callback();
+        };
+        if (newMapIds.length)
+            processMaps(worldData[w].id, newMapIds, 0, processCallback);
+        else
+            processCallback();
+        if (removedIds.length)
+            deleteRemovedMaps(removedIds);
+    });
+}
+
+function processMaps(worldId, mapIds, m, callback) {
+    let mapId = mapIds[m];
+    exec(`..\\..\\tools\\lcf2xml.exe ../in/Map${mapId}.lmu`, {
+        cwd: './maps/out/'
+    }, (err) => {
+        if (err) throw err;
+        fs.readFile(`./maps/out/Map${mapId}.emu`, function(err, data) {
+            if (err) throw err;
+            let insertCallback = function () {
+                if (++m < mapIds.length)
+                    processMaps(worldId, mapIds, m, callback);
+                else
+                    callback();
+            };
+            var json;
+            try {
+                json = JSON.parse(xmlJs.xml2json(data, {compact: true, spaces: 4}));
+            } catch (error) {
+                console.error(error, worldId, mapIds[m]);
+                insertCallback();
+                return;
+            }
+            let map = json.LMU.Map;
+            pool.query(`INSERT INTO maps (worldId, mapId, width, height) VALUES ('${worldId}', '${mapId}', ${parseInt(map.width._text)}, ${parseInt(map.height._text)})`, (err, _) => {
+                if (err) throw err;
+                insertCallback();
+            });
+         });
+    });
+}
+
+function deleteRemovedMaps(removedMapIds) {
+    var i = 0;
+    var deleteMapsQuery = "DELETE FROM maps WHERE id IN (";
+    for (var m in removedMapIds) {
+        if (i++)
+            deleteMapsQuery += ", ";
+        deleteMapsQuery += removedMapIds[m];
+    }
+    deleteMapsQuery += ")";
+    pool.query(deleteMapsQuery, (err, _) => {
+        if (err) return console.log(err);
     });
 }
 
@@ -436,6 +556,7 @@ function getWorldInfo(worldName, callback) {
         }
         callback({
             connections: getConnections(res.text),
+            mapIds: _.filter(getMapIds(res.text), (id) => !isNaN(id) && id.length === 4),
             filename: worldName + ext
         });
     });
@@ -457,26 +578,30 @@ function downloadImage(imageUrl, filename) {
 function getConnections(html) {
     let ret = [];
     html = html.slice(html.indexOf("<b>Connecting Areas</b>"), html.indexOf("<b>BGM</b>"));
-    areas = html.split(/(?:<p>|, +)<a href="/);
+    let areas = html.split(/(?:<p>|<br \/>)<a href="/);
     if (areas.length > 1) {
         for (var a = 1; a < areas.length; a++) {
             var connType = 0;
             let areaText = areas[a];
             let urlIndex = areaText.indexOf("/wiki/") + 6;
-            if (areaText.indexOf("NoReturn") > -1)
+            if (areaText.indexOf(">NoReturn<") > -1)
                 connType |= ConnType.ONE_WAY;
-            else if (areaText.indexOf("NoEntry") > -1)
+            else if (areaText.indexOf(">NoEntry<") > -1)
                 connType |= ConnType.NO_ENTRY;
-            if (areaText.indexOf("Unlock") > -1)
+            if (areaText.indexOf(">Unlock<") > -1)
                 connType |= ConnType.UNLOCK;
-            else if (areaText.indexOf("Locked") > -1)
+            else if (areaText.indexOf(">Locked<") > -1)
                 connType |= ConnType.LOCKED;
-            if (areaText.indexOf("DeadEnd") > -1)
+                else if (areaText.indexOf(">LockedCondition<") > -1)
+                connType |= ConnType.LOCKED_CONDITION;
+            if (areaText.indexOf(">DeadEnd<") > -1)
                 connType |= ConnType.DEAD_END;
-            else if (areaText.indexOf("Return") > -1)
+            else if (areaText.indexOf(">Return<") > -1)
                 connType |= ConnType.ISOLATED;
             if (areaText.indexOf("effect") > -1)
-                connType |= ConnType.EFFECT
+                connType |= ConnType.EFFECT;
+            if (areaText.indexOf(">Chance<") > -1)
+                connType |= ConnType.CHANCE;
             ret.push({
                 location: areaText.slice(urlIndex, areaText.indexOf('"', urlIndex)).replace(/%26/g, "&").replace(/%27/g, "'").replace(/\_/g, " ").replace(/#.*/, ""),
                 type: connType
@@ -484,6 +609,12 @@ function getConnections(html) {
         }
     }
     return ret;
+}
+
+function getMapIds(html) {
+    let ret = [];
+    html = html.slice(html.indexOf("<b>Map ID</b>"), html.indexOf("<b>Author</b>"));
+    return html.slice(html.indexOf("<p>") + 3, html.indexOf("</p>")).trim().split(", ");
 }
 
 app.listen(port, () => console.log(`2kki app listening on port ${port}!`))
